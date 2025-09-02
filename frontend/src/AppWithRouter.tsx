4800a1fe6132e41707d45dd07ab3ac351e5641ac
Sir-shkolnik <support@movedin.com>
2025-09-01 22:23:41 -0400

    EMERGENCY FIX: Revert to HashRouter - BrowserRouter broke routing, restore working hash navigation

--- frontend/src/AppWithRouter.tsx
+++ frontend/src/AppWithRouter.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 import App from './App';
 import AboutUs from './pages/AboutUs';
